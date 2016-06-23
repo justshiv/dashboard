141eaeae7b2c2b48ae0cf006f3210c3248de1573
justshiv <siobhan@justshiv.com>
2016-06-23 18:31:12 +0200

    setup basic react-redux application

---
+++ app/components/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Rechart from '../Rechart'
+
+const App = () => (
+  <div>
+    <Rechart />
+  </div>
+)
+
+export default App
---
+++ app/components/Rechart/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Rechart = () => (
+  <p> Rechart </p>
+)
+
+export default Rechart
